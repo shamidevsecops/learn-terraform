42bb8d2b0945d28bbcbc1fc6d016475d6bfd289f
Shami2810 <shami.ssk2810@gmail.com>
2025-12-23 19:44:18 +0530

    roboshop terraform code

---
+++ 02.variables/sample.tf
@@ -0,0 +1,13 @@
+variable "x1" {
+  default = "abc"
+}
+
+output "x1" {
+  # variable var.x1 for the direct variable usage
+  value = "var.x1"
+}
+
+output "x11" {
+  # variable var.x1 is with combination of a string then ${var.x1}
+    value ="Value of $x1 is ${}var.x1}"
+}
