4d95dfeae8d254bb2ac860c3ee4d96fb3ff2036a
Shami2810 <shami.ssk2810@gmail.com>
2025-12-24 12:24:07 +0530

    learn terraform code

--- 02.variables/sample.tf
+++ 02.variables/sample.tf
@@ -15,4 +15,53 @@ output "x11" {
   value = "Value of x1 is ${var.x1}"
 }
 
+# Data Types
+# String , Need to be given in double quotes
+# Number, No need of quote
+# Boolean, No need of quote
+
+variable "string1" {
+  default = "abc"
+}
+
+variable "number1" {
+  default = 100
+}
+
+variable "number2" {
+  default =100.1
+}
+
+# true or false is supported.
+variable "boolean1" {
+  default = true
+}
+# Variable Types
+# Plain
+# List
+# Map / Dict
+
+variable "plain1" {
+  default = "abc"
+}
+
+# list index starts from 0
+variable "list1" {
+  default = ["abc", 123, true]
+}
+
+variable "map1" {
+  default = {
+    name = "devops"
+    skill = "engineer"
+  }
+}
+
+output "list1_value1" {
+  value = var.list1[0]
+}
+
+output "map1_name" {
+  value = var.map1["name"]
+}
 
