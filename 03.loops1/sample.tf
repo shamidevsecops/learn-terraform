dddb3a0ce48aedc21767ab0e2d540506f63ec615
Shami2810 <shami.ssk2810@gmail.com>
2025-12-25 08:18:19 +0530

    learn terraform code

--- 03.loops1/sample.tf
+++ 03.loops1/sample.tf
@@ -1,3 +1,15 @@
 resource "null_resource" "dummy" {
 count = 10
 }
+
+provider "aws" {}
+
+resource "aws_instance" "web" {
+  count = 2
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
