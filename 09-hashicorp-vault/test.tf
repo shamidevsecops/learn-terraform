01febf8055706c927a40b31ac03997956172266b
Shami2810 <shami.ssk2810@gmail.com>
2025-12-27 10:01:55 +0530

    learn terraform code

---
+++ 09-hashicorp-vault/test.tf
@@ -0,0 +1,19 @@
+data "vault_generic_secret" "sample" {
+  path = "kv/demo"
+}
+
+# Rundeck Provider, for example
+# For this example, in Vault there is a key named "auth_token" and the value is the token we need to keep secret.
+# In general usage, replace "auth_token" with the key you wish to extract from Vault.
+
+provider "vault" {
+  address        = "http:/44.211.83.52:8200/"
+  token = var.token
+}
+
+variable "token" {}
+
+output "secret" {
+  value = data.vault_generic_secret.sample
+  sensitive = true
+}
